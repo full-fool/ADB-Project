929f4cf229964aa236cf000c79dcf2d35e444498
full-fool <cyq_2210@163.com>
2016-12-04 19:13:30 -0500

    update preprocess

--- project III/preprocess.py
+++ project III/preprocess.py
@@ -1,27 +1,29 @@
+import csv
 # These lines store the mapping between restaurant name and their problems.
 rest_dict = {}
 
-for line in open('DOHMH_New_York_City_Restaurant_Inspection_Results.csv'):
-	line_list = line.split(',')
+f = open('New_York_City_Restaurant_Inspection_Results.csv', 'rb') # opens the csv file
+try:
+    reader = csv.reader(f)  # creates the reader object
+    for line_list in reader:   # iterates the rows of the file in orders
 
-	# Removes incomplete lines to increse confidence.
-	if line_list[10] == '':
-	 	continue
-	if not line_list[0] in rest_dict:
-		#rest_dict[line_list[0]] = set([line_list[2], line_list[7], line_list[10]])
-		#rest_dict[line_list[0]] = set([line_list[7], line_list[10]])
-		rest_dict[line_list[0]] = set([line_list[10]])
-	else:
-		#rest_dict[line_list[0]].add(line_list[2])
-		#rest_dict[line_list[0]].add(line_list[7])
-		rest_dict[line_list[0]].add(line_list[10])
+		if not line_list[1] in rest_dict:
+			rest_dict[line_list[1]] = set([line_list[10]])
+		else:
+			rest_dict[line_list[1]].add(line_list[10])
+
+
+
+finally:
+    f.close()     
+
 
 target_file = open('New_York_City_Restaurant_Inspection_Results_Concern.csv', 'w')
 for restaurant in rest_dict.keys():
 
 	# We only concern restaurants who has no less than 10 problems to 
 	# dig their assocation rules.
-	if len(rest_dict[restaurant]) < 5:
+	if len(rest_dict[restaurant]) < 10:
 		continue
 	target_file.write('%s\n' % (','.join(list(rest_dict[restaurant]))))
 
